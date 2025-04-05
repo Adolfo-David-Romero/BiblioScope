3b5557d421002ba134ae15fd03fc08f1a4cc8b4d
Adolfo David Romero <Romeroad@Sheridancollege.ca>
2025-04-05 00:43:40 -0400

    Added Book class with basic properties

---
+++ BiblioScope/Model/Book.cs
@@ -0,0 +1,55 @@
+namespace BiblioScope.Model;
+
+public class Book
+{
+    //fields
+    private string _isbn;
+    private string _title;
+    private string _author;
+    private string _publisher;
+    private string _genre;
+    
+    /// <summary> Represents a singular Book. </summary>
+    public Book(string isbn, string title, string author, string publisher, string genre)
+    {
+        _isbn = isbn;
+        _title = title;
+        _author = author;
+        _publisher = publisher;
+        _genre = genre;
+    }
+    
+    //Properties
+    public string Isbn
+    {
+        get => _isbn;
+        set => _isbn = value; //TODO: Add exeption handling 
+    }
+
+    public string Title
+    {
+        get => _title;
+        set => _title = value;
+    }
+
+    public string Author //TODO: Figure out what happens when there's multiple authors
+    {
+        get => _author;
+        set => _author = value;
+    }
+
+    public string Publisher
+    {
+        get => _publisher;
+        set => _publisher = value;
+    }
+
+    public string Genre
+    {
+        get => _genre;
+        set => _genre = value;
+    }
+    
+    //Methods
+    
+}
