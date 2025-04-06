d2390a432c948ef34cb1e52f3517223c97c8104f
Adolfo David Romero <Romeroad@Sheridancollege.ca>
2025-04-06 13:18:03 -0400

    Added basic routing to pages with svg icons added to resources. Added empty content view pages for later reusable UI components.

---
+++ BiblioScope/View/GenreBubble.xaml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblioScope.View;
+
+/// <summary>Reusable UI component for genre icons </summary>
+public partial class GenreBubble : ContentView
+{
+    public GenreBubble()
+    {
+        InitializeComponent();
+    }
+}
